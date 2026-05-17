d46c4af96671d1b2b9cd320481a071df98ac99c0
ranasarim814-blip <ranasarim814@gmail.com>
2026-05-17 14:14:04 +0500

    ready for deployement

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
 // start server
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: "https://beion-marketing-web.vercel.app/"
+}));
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
